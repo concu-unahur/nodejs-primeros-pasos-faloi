d883ba90699548cb5e4163a53593afb007406713
Federico Aloi <federico.aloi@gmail.com>
2020-03-03 20:44:18 -0300

    Algunas funciones con callbacks

--- callback.js
+++ callback.js
@@ -2,6 +2,26 @@ function siguiente(x, next) {
   next(x + 1)
 }
 
-siguiente(2, resultado => { 
-  console.log('Siguiente de 8 =>', resultado)
-})
+function doble(x, next) {
+  next(x * 2)
+}
+
+function imprimir(resultado) {
+  console.log('Resultado =>', resultado)
+}
+
+function calcularSiguienteEImprimir(elDoble) {
+  siguiente(elDoble, imprimir)
+}
+
+siguiente(8, imprimir)
+doble(8, imprimir)
+doble(8, calcularSiguienteEImprimir)
+
+// Siguiente del doble, usando funciones anónimas:
+
+// doble(8, elDoble => {
+//   siguiente(elDoble, elSiguienteDelDoble => {
+//     console.log('Resultado =>', elSiguienteDelDoble)
+//   })
+// })
